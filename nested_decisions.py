a28d6ee1a40d1622dbe1dfe9d7f16c9a7459ed2c
b-smith94 <byrondsmith9@gmail.com>
2024-07-28 13:28:54 -0400

    added pass function for invalid answers to if statements

--- nested_decisions.py
+++ nested_decisions.py
@@ -1,14 +1,20 @@
 place = input("Choose a place: forest or cave? ")
 
 if place == "forest": #need to change = to ==
-    action = input("climb a tree or cross a river?") 
+    action = input("climb a tree or cross a river? ") 
     if action == "climb a tree": #need to change = to ==
         print("You found a bird's nest!") 
-    else: #remove variable--not needed for else statement
+    elif action == "cross a river":  #change else to elif
         print("You found a boat!")
+    else:
+        pass
 elif place == "cave": #change = to ==
     action = input("Do you light a torch or proceed in the dark? ")
     if action == "light a torch":
         print("You easily spot the treasure in the now well-lit cavern!")
-    else:
+    elif action == "proceed in the dark":
         print("You fumble in the dark for hours but cannot find a way forward.")
+    else:
+        pass
+else:
+    pass
